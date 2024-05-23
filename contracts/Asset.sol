6f2ad54be9312e27d6745e0834d4e40c7120854a
ziankork <zian@cork.technology>
2024-05-23 18:37:42 +0800

    fix: add correct pragma

--- contracts/Asset.sol
+++ contracts/Asset.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
