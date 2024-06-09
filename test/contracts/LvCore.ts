824724e48dfea80dfc5afe265dc0d2a659460b12
ziankork <zian@cork.technology>
2024-06-09 23:50:14 +0800

    test: added lv test

---
+++ test/contracts/LvCore.ts
@@ -0,0 +1,14 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+import { Address, formatEther, parseEther, WalletClient } from "viem";
+import * as helper from "../helper/TestHelper";
+
+describe("LvCore", function () {
+  it("should deposit", async function () {});
+  it("should redeem expired", async function () {});
+  it("should redeem early", async function () {});
+});
