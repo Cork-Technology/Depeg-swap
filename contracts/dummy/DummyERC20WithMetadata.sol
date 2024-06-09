739fad00ad7be05a062a23b34912f91a017ec431
ziankork <zian@cork.technology>
2024-06-10 01:50:25 +0800

    chore: remove dev deps

--- contracts/dummy/DummyERC20WithMetadata.sol
+++ contracts/dummy/DummyERC20WithMetadata.sol
@@ -3,17 +3,16 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
-import "../interfaces/dev/IDevToken.sol";
 
 // dummy contract for RA and PA
-contract DummyERCWithMetadata is ERC20Burnable, IDevToken {
+contract DummyERCWithMetadata is ERC20Burnable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-    function mint(address to, uint256 amount) external override {
+    function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
 
-    function burnSelf(uint256 amount) external override {
+    function burnSelf(uint256 amount) external {
         _burn(msg.sender, amount);
     }
 }
--- contracts/interfaces/dev/IDevToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-interface IDevToken {
-    function mint(address to, uint256 amount) external;
-
-    function burnSelf(uint256 amount) external;
-}
