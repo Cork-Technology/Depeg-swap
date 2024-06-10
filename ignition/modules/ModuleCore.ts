7edcd16a3bbb4e1af06768103e1a3a40dec38b03
ziankork <zian@cork.technology>
2024-06-11 01:08:43 +0800

    fix: add missing ignition

---
+++ ignition/modules/ModuleCore.ts
@@ -0,0 +1,17 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import assetFactory from "./AssetFactory";
+
+const ModuleCore = buildModule("ModuleCore", (m) => {
+  const { contract } = m.useModule(assetFactory);
+
+  const mathHelper = m.library("MathHelper");
+  const module = m.contract("ModuleCore", [contract], {
+    libraries: {
+      MathHelper: mathHelper,
+    },
+  });
+
+  return { module };
+});
+
+export default ModuleCore;
