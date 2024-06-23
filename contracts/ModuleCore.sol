4dfef9496eb856fd04d619d276f5a818b21926f7
ziankork <zian@cork.technology>
2024-06-23 16:17:40 +0800

    feat: add getters for last dsId

--- contracts/ModuleCore.sol
+++ contracts/ModuleCore.sol
@@ -87,4 +87,10 @@ contract ModuleCore is PsmCore, Initialize, VaultCore {
 
         emit Issued(id, idx, expiry, _ds, _ct);
     }
+
+    function lastDsId(
+        Id id
+    ) external view override returns (uint256 dsId) {
+        return states[id].globalAssetIdx;
+    }
 }
