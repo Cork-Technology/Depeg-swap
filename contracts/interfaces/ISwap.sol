670a5b315f22964e9a5d7d2f59f44751b8186d72
ziankork <zian@cork.technology>
2024-07-17 19:59:03 +0800

    feat: added basic interface

---
+++ contracts/interfaces/ISwap.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "../libraries/Pair.sol";
+
+interface ISwapDs {
+    function buy(
+        Id id,
+        uint256 dsId,
+        uint256 amount
+    ) external returns (uint256 received);
+
+    function currentPrice(Id id, uint256 dsId) external view returns (uint256);
+
+    function sell(
+        Id id,
+        uint256 dsId,
+        uint256 amount
+    ) external returns (uint256 received);
+
+    function previewSell(
+        Id id,
+        uint256 dsId,
+        uint256 amount
+    ) external view returns (uint256 received);
+
+    function previewBuy(
+        Id id,
+        uint256 dsId,
+        uint256 amount
+    ) external view returns (uint256 received);
+
+}
