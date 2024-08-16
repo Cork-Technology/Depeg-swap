c9124fb5b111936a7cf05eb9e443b53026d4e3a2
ziankork <zian@cork.technology>
2024-08-16 19:27:14 +0800

    feat: add core ignition

--- ignition/modules/core.ts
+++ ignition/modules/core.ts
@@ -1,5 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { flashSwapMath, mathHelper } from "./lib";
+import { flashSwapMath, mathHelper, vaultLib } from "./lib";
+import { uniV2Factory, uniV2router } from "./uniV2";
+import moduleCore from "../../artifacts/contracts/core/ModuleCore.sol/ModuleCore.json";
 
 export const assetFactory = buildModule("AssetFactory", (m) => {
   const contract = m.contract("AssetFactory");
@@ -24,4 +26,31 @@ export const flashSwapRouter = buildModule("FlashSwapRouter", (m) => {
   });
 
   return { FlashSwapRouter: contract };
+});
+
+export const ModuleCore = buildModule("ModuleCore", (m) => {
+  const { MathHelper } = m.useModule(mathHelper);
+  const { VaultLibrary } = m.useModule(vaultLib);
+
+  const _assetFactory = m.getParameter("assetFactory");
+  const _uniV2Factory = m.getParameter("uniV2Factory");
+  const _flashSwapRouter = m.getParameter("flashSwapRouter");
+  const _uniV2Router = m.getParameter("uniV2Router");
+  const _corkConfig = m.getParameter("corkConfig");
+
+  const contract = m.contract(
+    "ModuleCore",
+    moduleCore,
+    [_assetFactory, _uniV2Factory, _flashSwapRouter, _uniV2Router, _corkConfig],
+    {
+      libraries: {
+        MathHelper: MathHelper,
+        VaultLibrary: VaultLibrary,
+      },
+    }
+  );
+
+  return {
+    ModuleCore: contract,
+  };
 });
