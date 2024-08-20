2a5650960f593d765a8093f4606fe822ff213b7e
ziankork <zian@cork.technology>
2024-08-20 13:17:00 +0800

    chore: remove useless comment

--- contracts/libraries/MutexLock.sol
+++ contracts/libraries/MutexLock.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.24;
 
-// right now we're using regular storage instead of transient storage
 library MutexLock {
     // The slot holding the locked state, transiently. bytes32(uint256(keccak256("Locked")) - 1)
     bytes32 internal constant IS_LOCKED_SLOT = 0x10bc01d611f2c803e55ff98d999836f196afae7cf91f96f3b6f511e56ba84973;
