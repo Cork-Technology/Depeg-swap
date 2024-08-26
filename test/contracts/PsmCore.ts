1dab45dd324cb55c10b0712464d9462a809409b9
ziankork <zian@cork.technology>
2024-08-26 20:10:07 +0800

    chore: adjust tests

--- test/contracts/PsmCore.ts
+++ test/contracts/PsmCore.ts
@@ -962,71 +962,79 @@ describe("PSM core", function () {
     }
   });
 
-  it("should separate liquidity", async function () {
-    mintAmount = parseEther("10000");
+  describe("separate liquidity", function () {
+    it("should separate liquidity", async function () {
+      mintAmount = parseEther("10000");
 
-    await fixture.ra.write.approve([fixture.moduleCore.address, mintAmount]);
+      await fixture.ra.write.approve([fixture.moduleCore.address, mintAmount]);
 
-    await helper.mintRa(
-      fixture.ra.address,
-      defaultSigner.account.address,
-      mintAmount
-    );
+      await helper.mintRa(
+        fixture.ra.address,
+        defaultSigner.account.address,
+        mintAmount
+      );
 
-    pa.write.approve([fixture.moduleCore.address, parseEther("10000")]);
+      pa.write.approve([fixture.moduleCore.address, parseEther("10000")]);
 
-    const expiry = helper.expiry(expiryTime);
+      const expiry = helper.expiry(expiryTime);
 
-    await issueNewSwapAssets(expiry);
+      await issueNewSwapAssets(expiry);
 
-    await fixture.moduleCore.write.depositPsm([fixture.Id, parseEther("1000")]);
-    await time.increaseTo(expiry);
+      await fixture.moduleCore.write.depositPsm([
+        fixture.Id,
+        parseEther("1000"),
+      ]);
+      await time.increaseTo(expiry);
 
-    const newExpiry = helper.expiry(expiry * 2);
+      const newExpiry = helper.expiry(expiry * 2);
 
-    const { dsId, ct } = await helper.issueNewSwapAssets({
-      expiry: newExpiry,
-      moduleCore: fixture.moduleCore.address,
-      config: fixture.config.contract.address,
-      pa: fixture.pa.address,
-      ra: fixture.ra.address,
-      factory: fixture.factory.contract.address,
+      const { dsId, ct } = await helper.issueNewSwapAssets({
+        expiry: newExpiry,
+        moduleCore: fixture.moduleCore.address,
+        config: fixture.config.contract.address,
+        pa: fixture.pa.address,
+        ra: fixture.ra.address,
+        factory: fixture.factory.contract.address,
+      });
+
+      await fixture.moduleCore.write.depositPsm([
+        fixture.Id,
+        parseEther("1000"),
+      ]);
+
+      const deadline = BigInt(newExpiry * 2);
+
+      const msgPermit = await helper.permit({
+        amount: parseEther("100"),
+        deadline,
+        erc20contractAddress: ct!,
+        psmAddress: fixture.moduleCore.address,
+        signer: defaultSigner,
+      });
+
+      await time.increaseTo(newExpiry);
+
+      await fixture.moduleCore.write.redeemWithCT([
+        fixture.Id,
+        dsId!,
+        parseEther("100"),
+        msgPermit,
+        deadline,
+      ]);
+
+      const event = await fixture.moduleCore.getEvents
+        .CtRedeemed({
+          Id: fixture.Id,
+          redeemer: defaultSigner.account.address,
+          dsId,
+        })
+        .then((e) => e[0]);
+
+      // we expect the amount to be 100 because we deposit 1000 on the DS id while having a total
+      // of 2000 RA on the PSM including the DS ID before this, so if we get 1/10 of 1000 back from what
+      // we deposited on this DS ID, we can consider this a success
+      expect(event.args.raReceived!).to.equal(parseEther("100"));
     });
-
-    await fixture.moduleCore.write.depositPsm([fixture.Id, parseEther("1000")]);
-
-    const deadline = BigInt(newExpiry * 2);
-
-    const msgPermit = await helper.permit({
-      amount: parseEther("100"),
-      deadline,
-      erc20contractAddress: ct!,
-      psmAddress: fixture.moduleCore.address,
-      signer: defaultSigner,
-    });
-
-    time.increaseTo(newExpiry);
-
-    await fixture.moduleCore.write.redeemWithCT([
-      fixture.Id,
-      dsId!,
-      parseEther("100"),
-      msgPermit,
-      deadline,
-    ]);
-
-    const event = await fixture.moduleCore.getEvents
-      .CtRedeemed({
-        Id: fixture.Id,
-        redeemer: defaultSigner.account.address,
-        dsId,
-      })
-      .then((e) => e[0]);
-
-    // we expect the amount to be 100 because we deposit 1000 on the DS id while having a total
-    // of 2000 RA on the PSM including the DS ID before this, so if we get 1/10 of 1000 back from what
-    // we deposited on this DS ID, we can consider this a success
-    expect(event.args.raReceived!).to.equal(parseEther("100"));
   });
 
   describe("valueLocked", function () {
