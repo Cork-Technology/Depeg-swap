47f8e7c47e1b2b039bdc1e5a718c3b406834d99d
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-08-28 21:47:57 +0530

    Fix: improved CST and CETH contract

--- contracts/tokens/CETH.sol
+++ contracts/tokens/CETH.sol
@@ -1,14 +1,21 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.24;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-// CETH Contract
+/**
+ * @title CETH Contract
+ * @author Cork Team
+ * @notice CETH contract represents Cork ETH
+ */
 contract CETH is ERC20, Ownable {
-    constructor() ERC20("Cork ETH", "CETH") Ownable(msg.sender){}
+    constructor() ERC20("Cork ETH", "CETH") Ownable(msg.sender) {}
 
-    // Minting function, only the owner can mint
+    /**
+     * @dev Function for minting new Cork ETH(Only owners can mint)
+     * @param to Address of destination wallet
+     * @param amount number of ETH to be minted
+     */
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
--- contracts/tokens/CST.sol
+++ contracts/tokens/CST.sol
@@ -1,12 +1,15 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.24;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// CST Contract
+/**
+ * @title CST Contract
+ * @author Cork Team
+ * @notice CST contract represents Staked Ethereum
+ */
 contract CST is ERC20, Ownable {
     using SafeERC20 for IERC20;
 
@@ -14,18 +17,25 @@ contract CST is ERC20, Ownable {
     address public admin;
 
     /* ========== EVENTS ========== */
+    /// @notice error thrown when Address is Zero Address
     error ZeroAddressNotAllowed();
+
+    /// @notice error thrown when passed Amount is zero
     error ZeroAmountNotAllowed();
+
+    /// @notice error thrown when CETH Balance is not sufficient
     error InsufficientBalance();
+
+    /// @notice error thrown when user don't have enough CST
     error InsufficientCSTBalance();
+
+    /// @notice error thrown when total supply of CST is 0
     error ZeroCSTSupply();
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        address _ceth,
-        address _admin
-    ) ERC20(name, symbol) Ownable(_admin){
+    constructor(string memory name, string memory symbol, address _ceth, address _admin)
+        ERC20(name, symbol)
+        Ownable(_admin)
+    {
         if (_ceth == address(0)) {
             revert ZeroAddressNotAllowed();
         }
@@ -33,25 +43,31 @@ contract CST is ERC20, Ownable {
         admin = _admin;
     }
 
-    // Deposit CETH and receive CST tokens at a 1:1 ratio
+    /**
+     * @dev Deposit CETH and receive CST tokens at a 1:1 ratio
+     * @param amount number of Cork ETH to be deposited and staked ETH to be minted
+     */
     function deposit(uint256 amount) public {
-        if(amount == 0){
+        if (amount == 0) {
             revert ZeroAmountNotAllowed();
         }
         ceth.safeTransferFrom(msg.sender, address(this), amount);
         _mint(msg.sender, amount);
     }
 
-    // User can withdraw their pro-rata share of CETH by burning CST tokens
+    /**
+     * @dev User can withdraw their pro-rata share of CETH by burning CST tokens
+     * @param amount number of Cork ETH to be withdrawn
+     */
     function withdraw(uint256 amount) public {
-        if(amount == 0){
+        if (amount == 0) {
             revert ZeroAmountNotAllowed();
         }
-        if(amount > balanceOf(msg.sender)){
-            revert InsufficientCSTBalance(); 
+        if (amount > balanceOf(msg.sender)) {
+            revert InsufficientCSTBalance();
         }
         uint256 totalCSTSupply = totalSupply();
-        if(totalCSTSupply == 0){
+        if (totalCSTSupply == 0) {
             revert ZeroCSTSupply();
         }
 
@@ -65,10 +81,13 @@ contract CST is ERC20, Ownable {
         ceth.safeTransfer(msg.sender, cethAmount);
     }
 
-    // Admin can slash CETH from the contract
+    /**
+     * @dev Admin can slash CETH from the contract
+     * @param amount number of ETH to be minted
+     */
     function slash(uint256 amount) public onlyOwner {
-        if(amount > ceth.balanceOf(address(this))){
-            revert InsufficientBalance(); 
+        if (amount > ceth.balanceOf(address(this))) {
+            revert InsufficientBalance();
         }
         ceth.safeTransfer(admin, amount);
     }
