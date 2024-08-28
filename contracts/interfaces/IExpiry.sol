b05dd9eb712b8e1ac95974b832a4339a29eaf86d
ziankork <zian@cork.technology>
2024-08-28 21:41:24 +0800

    chore: add is expoired comments

--- contracts/interfaces/IExpiry.sol
+++ contracts/interfaces/IExpiry.sol
@@ -9,6 +9,7 @@ interface IExpiry {
     /// @notice Trying to issue an expired asset
     error Expired();
 
+    /// @notice returns true if the asset is expired
     function isExpired() external view returns (bool);
 
     ///@notice returns the expiry timestamp if 0 then it means it never expires
