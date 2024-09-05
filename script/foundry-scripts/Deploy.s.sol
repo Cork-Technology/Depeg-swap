2d4b696f370c4cfd14445ae1d028c9671e7ef3e4
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-09-05 11:18:58 +0530

    fix: Added foundry built bytecode for uniswap

--- script/foundry-scripts/Deploy.s.sol
+++ script/foundry-scripts/Deploy.s.sol
@@ -5,9 +5,15 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 import {AssetFactory} from "../../contracts/core/assets/AssetFactory.sol";
 import {CorkConfig} from "../../contracts/core/CorkConfig.sol";
 import {RouterState} from "../../contracts/core/flash-swaps/FlashSwapRouter.sol";
-// import {UniswapV2Factory} from "uniswap-v2/contracts/UniswapV2Factory.sol";
+import {IUniswapV2Factory} from "uniswap-v2/contracts/interfaces/IUniswapV2Factory.sol";
+
+string constant v2FactoryArtifact = "test/helper/ext-abi/uni-v2-factory.json";
+string constant v2RouterArtifact = "test/helper/ext-abi/uni-v2-router.json";
 
 contract DeployScript is Script {
+    IUniswapV2Factory public factory;
+    IUniswapV2Router public univ2Router;
+
     AssetFactory public assetFactory;
     CorkConfig public config;
     RouterState public flashswapRouter;
@@ -22,29 +28,40 @@ contract DeployScript is Script {
 
         // Deploy the Asset Factory implementation (logic) contract
         AssetFactory assetFactoryImplementation = new AssetFactory();
+        console.log("Asset Factory Implementation    : ", address(assetFactoryImplementation));
 
         // Deploy the Asset Factory Proxy contract
         bytes memory data = abi.encodeWithSelector(assetFactoryImplementation.initialize.selector);
         ERC1967Proxy assetFactoryProxy = new ERC1967Proxy(address(assetFactoryImplementation), data);
         assetFactory = AssetFactory(address(assetFactoryProxy));
+        console.log("Asset Factory                   : ", address(assetFactory));
 
         // Deploy the CorkConfig contract
         config = new CorkConfig();
+        console.log("Cork Config                     : ", address(config));
 
         // Deploy the FlashSwapRouter implementation (logic) contract
         RouterState routerImplementation = new RouterState();
+        console.log("Flashswap Router Implementation : ", address(routerImplementation));
 
         // Deploy the FlashSwapRouter Proxy contract
         data = abi.encodeWithSelector(routerImplementation.initialize.selector);
         ERC1967Proxy routerProxy = new ERC1967Proxy(address(routerImplementation), data);
         flashswapRouter = RouterState(address(routerProxy));
+        console.log("Flashswap Router Proxy          : ", address(flashswapRouter));
 
         // Transfer Ownership to moduleCore
         assetFactory.transferOwnership(moduleCore);
         flashswapRouter.transferOwnership(moduleCore);
+        console.log("Transferred ownerships to Modulecore");
 
         // Deploy the UniswapV2Factory contract
-        // UniswapV2Factory uniV2Factory = new UniswapV2Factory(msg.sender, flashSwapRouterAddress);
+        address _factory = deployCode(v2FactoryArtifact, abi.encode(msg.sender, address(flashswapRouter)));
+        factory = IUniswapV2Factory(_factory);
+
+        // Deploy the UniswapV2Router contract
+        address _router = deployCode(v2FactoryArtifact, abi.encode(msg.sender, address(flashswapRouter)));
+        univ2Router = IUniswapV2Factory(_router);
         vm.stopBroadcast();
     }
 }
