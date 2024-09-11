bb1b65018dd2e6c637c5c4bc6042bf06c3e11ba9
ziankork <zian@cork.technology>
2024-09-12 01:29:34 +0800

    fix: use uq112x112

--- contracts/libraries/DsSwapperMathLib.sol
+++ contracts/libraries/DsSwapperMathLib.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.24;
 import {UQ112x112} from "./UQ112x112.sol";
 import {SignedMath} from "@openzeppelin/contracts/utils/math/SignedMath.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {UQ112x112} from "./UQ112x112.sol";
 
 /**
  * @title SwapperMathLibrary Contract
@@ -23,6 +24,9 @@ library SwapperMathLibrary {
 
     /// @notice thrown when Output amount is not sufficient
     error InsufficientOutputAmount();
+
+    /// @notice thrown when the number is too big
+    error TooBig();
 
     // Calculate price ratio of two tokens in a uniswap v2 pair, will return ratio on 18 decimals precision
     function getPriceRatioUniv2(uint112 raReserve, uint112 ctReserve)
@@ -107,7 +111,11 @@ library SwapperMathLibrary {
      *   - r: RA needed to borrow from AMM
      *
      */
-    function getAmountOutDs(int256 raReserve, int256 ctReserve, int256 raProvided) external pure returns (uint256 raBorrowed, uint256 dsReceived) {
+    function getAmountOutDs(int256 raReserve, int256 ctReserve, int256 raProvided)
+        external
+        pure
+        returns (uint256 raBorrowed, uint256 dsReceived)
+    {
         // first we solve the sqrt part of the equation first
 
         // E^2
@@ -138,24 +146,39 @@ library SwapperMathLibrary {
         raBorrowed = dsReceived - SignedMath.abs(raProvided);
     }
 
-    function calculatePrecentage(uint256 amount, uint256 precentage) private pure returns(uint256 result){
+    function calculatePrecentage(uint256 amount, uint256 precentage) private pure returns (uint256 result) {
         result = (((amount * 1e18) * precentage) / (100 * 1e18)) / 1e18;
     }
 
     /**
      * cumulatedHPA = Price_i × Volume_i × 1 - ((Discount / 86400) * (currentTime - issuanceTime))
      */
-    function calculateHPAcumulated(uint256 effectiveDsPrice, uint256 amount, uint256 decayDiscountInDays, uint256 issuanceTimestamp, uint256 currentTime) external pure returns(uint256 cumulatedHPA){
+    function calculateHPAcumulated(
+        uint256 effectiveDsPrice,
+        uint256 amount,
+        uint256 decayDiscountInDays,
+        uint256 issuanceTimestamp,
+        uint256 currentTime
+    ) external pure returns (uint256 cumulatedHPA) {
         uint256 decay = calculateDecayDiscount(decayDiscountInDays, issuanceTimestamp, currentTime);
 
-        cumulatedHPA = calculatePrecentage(effectiveDsPrice * amount / 1e18, decay) ;
+        cumulatedHPA = calculatePrecentage(effectiveDsPrice * amount / 1e18, decay);
     }
 
-    function calculateEffectiveDsPrice(uint256 dsAmount, uint256 raProvided) external pure returns(uint256 effectiveDsPrice){
+    function calculateEffectiveDsPrice(uint256 dsAmount, uint256 raProvided)
+        external
+        pure
+        returns (uint256 effectiveDsPrice)
+    {
         effectiveDsPrice = raProvided * 1e18 / dsAmount;
     }
 
-    function calculateVHPAcumulated(uint256 amount, uint256 decayDiscountInDays, uint256 issuanceTimestamp, uint256 currentTime) external pure returns (uint256 cumulatedVHPA) {
+    function calculateVHPAcumulated(
+        uint256 amount,
+        uint256 decayDiscountInDays,
+        uint256 issuanceTimestamp,
+        uint256 currentTime
+    ) external pure returns (uint256 cumulatedVHPA) {
         uint256 decay = calculateDecayDiscount(decayDiscountInDays, issuanceTimestamp, currentTime);
 
         cumulatedVHPA = calculatePrecentage(amount, decay);
@@ -170,17 +193,36 @@ library SwapperMathLibrary {
      * requirements :
      * Discount * (currentTime - issuanceTime) < 100
      */
-    function calculateDecayDiscount(uint256 decayDiscountInDays, uint256 issuanceTime, uint256 currentTime) public pure returns (uint256 decay) {
-        uint256 discPerSec = decayDiscountInDays / 1 days;
+    function calculateDecayDiscount(uint256 decayDiscountInDays, uint256 issuanceTime, uint256 currentTime)
+        public
+        pure
+        returns (uint256 decay)
+    {
+        if (decayDiscountInDays > type(uint112).max) {
+            revert TooBig();
+        }
+
+        uint224 discPerSec = UQ112x112.encode(uint112(decayDiscountInDays)) / 1 days;
         uint256 t = currentTime - issuanceTime;
-        uint256 discount = discPerSec * t;
+        uint256 discount = (discPerSec * t / UQ112x112.Q112) + 1;
 
         // this must hold true, it doesn't make sense to have a discount above 100%
         assert(discount < 100e18);
         decay = 100e18 - discount;
     }
 
-    function calculateRolloverSale(uint256 lvDsReserve, uint256 psmDsReserve, uint256 raProvided, uint256 hpa) external pure returns (uint256 lvProfit, uint256 psmProfit, uint256 raLeft, uint256 dsReceived, uint256 lvReserveUsed, uint256 psmReserveUsed) {
+    function calculateRolloverSale(uint256 lvDsReserve, uint256 psmDsReserve, uint256 raProvided, uint256 hpa)
+        external
+        view
+        returns (
+            uint256 lvProfit,
+            uint256 psmProfit,
+            uint256 raLeft,
+            uint256 dsReceived,
+            uint256 lvReserveUsed,
+            uint256 psmReserveUsed
+        )
+    {
         uint256 totalDsReserve = lvDsReserve + psmDsReserve;
 
         // calculate the amount of DS user will receive
@@ -188,10 +230,12 @@ library SwapperMathLibrary {
 
         // returns the RA if, the total reserve cannot cover the DS that user will receive. this Ra left must subject to the AMM rates
         raLeft = totalDsReserve > dsReceived ? 0 : ((dsReceived - totalDsReserve) * 1e18) / hpa;
-        
+
+        console.log("raLeft         :", raLeft);
+
         // recalculate the DS user will receive, after the RA left is deducted
         raProvided -= raLeft;
-        dsReceived = raProvided * hpa / 1e18;
+        dsReceived = raProvided * 1e18 / hpa;
 
         // proportionally calculate how much DS should be taken from LV and PSM
         // e.g if LV has 60% of the total reserve, then 60% of the DS should be taken from LV
