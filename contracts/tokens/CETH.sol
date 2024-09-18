a6b92e9930b1d4bcafe71da2b311222764d9f327
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-09-18 14:01:15 +0530

    fix: fixed ceth contract

--- contracts/tokens/CETH.sol
+++ contracts/tokens/CETH.sol
@@ -12,6 +12,7 @@ import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 contract CETH is ERC20, AccessControl {
     // Define a new role identifier for minters
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     constructor() ERC20("Cork ETH", "CETH") {
         // Grant the contract deployer the default admin role: they can grant and revoke roles
@@ -19,6 +20,7 @@ contract CETH is ERC20, AccessControl {
 
         // Grant the contract deployer the minter role so they can mint initially
         _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(BURNER_ROLE, msg.sender);
     }
 
     /**
@@ -29,6 +31,16 @@ contract CETH is ERC20, AccessControl {
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
+
+    /**
+     * @dev Function for burning new Cork ETH (Only accounts with Burner_ROLE can burn)
+     * @param from Address of from wallet
+     * @param amount number of CETH to be burned
+     */
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+    }
+
 
     /**
      * @dev Grant MINTER_ROLE to a new account (Only admin can grant)
@@ -44,5 +56,21 @@ contract CETH is ERC20, AccessControl {
      */
     function removeMinter(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
         revokeRole(MINTER_ROLE, account);
+    }
+
+    /**
+     * @dev Grant BURNER_ROLE to a new account (Only admin can grant)
+     * @param account Address of the new burner
+     */
+    function addBurner(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(BURNER_ROLE, account);
+    }
+
+    /**
+     * @dev Revoke BURNER_ROLE from an account (Only admin can revoke)
+     * @param account Address of the burner to revoke
+     */
+    function removeBurner(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(BURNER_ROLE, account);
     }
 }
