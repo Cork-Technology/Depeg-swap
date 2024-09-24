3358f7c37e7a46a058b8e0cbf20289a1ec1b24a2
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-09-25 01:18:42 +0530

    script : Added scripts for increasing LvDeposit and AMM liquidity

---
+++ script/foundry-scripts/IncreaseLvDeposit.s.sol
@@ -0,0 +1,63 @@
+pragma solidity 0.8.24;
+
+import {IUniswapV2Router02} from "v2-periphery/interfaces/IUniswapV2Router02.sol";
+
+import {Script, console} from "forge-std/Script.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {AssetFactory} from "../../contracts/core/assets/AssetFactory.sol";
+import {CorkConfig} from "../../contracts/core/CorkConfig.sol";
+import {RouterState} from "../../contracts/core/flash-swaps/FlashSwapRouter.sol";
+import {ModuleCore} from "../../contracts/core/ModuleCore.sol";
+import {CETH} from "../../contracts/tokens/CETH.sol";
+import {CST} from "../../contracts/tokens/CST.sol";
+import {Id} from "../../contracts/libraries/Pair.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+interface ICST {
+    function deposit(uint256 amount) external;
+}
+
+contract LiquidityScript is Script {
+    IUniswapV2Router02 public univ2Router;
+    ModuleCore public moduleCore;
+
+    bool public isProd = vm.envBool("PRODUCTION");
+    uint256 public base_redemption_fee = vm.envUint("PSM_BASE_REDEMPTION_FEE_PRECENTAGE");
+    address public ceth = vm.envAddress("WETH");
+    uint256 public pk = vm.envUint("PRIVATE_KEY3");
+
+    address bsETH = 0xb194fc7C6ab86dCF5D96CF8525576245d0459ea9;
+    address lbETH = 0xF24177162B1604e56EB338dd9775d75CC79DaC2B;
+    address wamuETH = 0x38B61B429a3526cC6C446400DbfcA4c1ae61F11B;
+    address mlETH = 0xCDc1133148121F43bE5F1CfB3a6426BbC01a9AF6;
+
+    CETH cETH = CETH(ceth);
+
+    uint256 depositLVAmt = 1_000_000 ether;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast(pk);
+        cETH = CETH(ceth);
+
+        univ2Router = IUniswapV2Router02(0x363E8886E8FF30b6f6770712Cf4e758e2Bf3E353);
+        moduleCore = ModuleCore(0xe56565c208d0a8Ca28FB632aD7F6518f273B8B9f);
+        console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+        increaseLvDeposit(mlETH, 300_000 ether);
+        increaseLvDeposit(lbETH, 300_000 ether);
+        increaseLvDeposit(bsETH, 300_000 ether);
+        increaseLvDeposit(wamuETH, 500_000 ether);
+        console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        vm.stopBroadcast();
+    }
+
+    function increaseLvDeposit(address cst, uint256 liquidityAmt) public {
+        Id id = moduleCore.getId(cst, ceth);
+        cETH.approve(address(moduleCore), depositLVAmt);
+        moduleCore.depositLv(id, depositLVAmt);
+        console.log("LV Deposited");
+        console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    }
+}
