be90340ff77704ddb1b2d01fa9b9151bf2e3ffe7
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-10-15 04:23:32 +0530

    feat : Added tests and netspec comments for HedgeUnit

--- contracts/core/assets/HedgeUnit.sol
+++ contracts/core/assets/HedgeUnit.sol
@@ -6,15 +6,33 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IHedgeUnit} from "../../interfaces/IHedgeUnit.sol";
 
+/**
+ * @title HedgeUnit
+ * @notice This contract allows minting and dissolving HedgeUnit tokens in exchange for two underlying assets.
+ * @dev The contract uses OpenZeppelin's ERC20, ReentrancyGuard, and Ownable modules.
+ */
 contract HedgeUnit is ERC20, ReentrancyGuard, Ownable, IHedgeUnit {
     using SafeERC20 for IERC20;
 
+    /// @notice The ERC20 token representing the DS asset.
     IERC20 public immutable DS;
+
+    /// @notice The ERC20 token representing the PA asset.
     IERC20 public immutable PA;
 
+    /// @notice Flag to indicate if minting is currently paused.
     bool public mintingPaused;
+
+    /// @notice Maximum supply cap for minting HedgeUnit tokens.
     uint256 public mintCap;
 
+    /**
+     * @dev Constructor that sets the DS and PA tokens and initializes the mint cap.
+     * @param _DS Address of the DS token.
+     * @param _PA Address of the PA token.
+     * @param _pairName Name of the HedgeUnit pair.
+     * @param _mintCap Initial mint cap for the HedgeUnit tokens.
+     */
     constructor(address _DS, address _PA, string memory _pairName, uint256 _mintCap)
         ERC20(string(abi.encodePacked("Hedge Unit - ", _pairName)), string(abi.encodePacked("HU - ", _pairName)))
         Ownable(msg.sender)
@@ -24,6 +42,13 @@ contract HedgeUnit is ERC20, ReentrancyGuard, Ownable, IHedgeUnit {
         mintCap = _mintCap;
     }
 
+    /**
+     * @notice Mints HedgeUnit tokens by transferring the equivalent amount of DS and PA tokens.
+     * @dev The function checks for the paused state and mint cap before minting.
+     * @param amount The amount of HedgeUnit tokens to mint.
+     * @custom:reverts MintingPaused if minting is currently paused.
+     * @custom:reverts MintCapExceeded if the mint cap is exceeded.
+     */
     function mint(uint256 amount) external nonReentrant {
         if (mintingPaused) {
             revert MintingPaused();
@@ -39,6 +64,13 @@ contract HedgeUnit is ERC20, ReentrancyGuard, Ownable, IHedgeUnit {
         emit Mint(msg.sender, amount);
     }
 
+    /**
+     * @notice Dissolves HedgeUnit tokens and returns the equivalent amount of DS and PA tokens.
+     * @param amount The amount of HedgeUnit tokens to dissolve.
+     * @return dsAmount The amount of DS tokens returned.
+     * @return paAmount The amount of PA tokens returned.
+     * @custom:reverts InvalidAmount if the user has insufficient HedgeUnit balance.
+     */
     function dissolve(uint256 amount) external nonReentrant returns (uint256 dsAmount, uint256 paAmount) {
         if (amount > balanceOf(msg.sender)) {
             revert InvalidAmount();
@@ -56,6 +88,11 @@ contract HedgeUnit is ERC20, ReentrancyGuard, Ownable, IHedgeUnit {
         emit Dissolve(msg.sender, amount, dsAmount, paAmount);
     }
 
+    /**
+     * @notice Sets the paused state for minting.
+     * @param _paused The new paused state (true to pause, false to unpause).
+     * @custom:reverts InvalidValue if the paused state is not changed.
+     */
     function setMintingPaused(bool _paused) external onlyOwner {
         if (_paused == mintingPaused) {
             revert InvalidValue();
@@ -64,6 +101,11 @@ contract HedgeUnit is ERC20, ReentrancyGuard, Ownable, IHedgeUnit {
         emit MintingPausedSet(_paused);
     }
 
+    /**
+     * @notice Updates the mint cap.
+     * @param _newMintCap The new mint cap value.
+     * @custom:reverts InvalidValue if the mint cap is not changed.
+     */
     function updateMintCap(uint256 _newMintCap) external onlyOwner {
         if (_newMintCap == mintCap) {
             revert InvalidValue();
--- contracts/interfaces/IHedgeUnit.sol
+++ contracts/interfaces/IHedgeUnit.sol
@@ -2,24 +2,90 @@ pragma solidity 0.8.24;
 
 interface IHedgeUnit {
     // Events
+    /**
+     * @notice Emitted when a user mints new HedgeUnit tokens.
+     * @param minter The address of the user minting the tokens.
+     * @param amount The amount of HedgeUnit tokens minted.
+     */
     event Mint(address indexed minter, uint256 amount);
+
+    /**
+     * @notice Emitted when a user dissolves HedgeUnit tokens.
+     * @param dissolver The address of the user dissolving the tokens.
+     * @param amount The amount of HedgeUnit tokens dissolved.
+     * @param dsAmount The amount of DS tokens received.
+     * @param paAmount The amount of PA tokens received.
+     */
     event Dissolve(address indexed dissolver, uint256 amount, uint256 dsAmount, uint256 paAmount);
+
+    /**
+     * @notice Emitted when the minting paused state is changed.
+     * @param paused The new paused state (true if paused, false otherwise).
+     */
     event MintingPausedSet(bool paused);
+
+    /**
+     * @notice Emitted when the mint cap is updated.
+     * @param newMintCap The new mint cap value.
+     */
     event MintCapUpdated(uint256 newMintCap);
 
     // Errors
+
+    /// @notice Error indicating that minting is currently paused.
     error MintingPaused();
+
+    /// @notice Error indicating an invalid amount was provided.
     error InvalidAmount();
+
+    /// @notice Error indicating the mint cap has been exceeded.
     error MintCapExceeded();
+
+    /// @notice Error indicating an invalid value was provided.
     error InvalidValue();
 
     // Read functions
+    /**
+     * @notice Returns the current state of minting pause.
+     * @return paused Boolean indicating whether minting is currently paused.
+     */
     function mintingPaused() external view returns (bool);
+
+    /**
+     * @notice Returns the current mint cap.
+     * @return mintCap The maximum supply cap for minting HedgeUnit tokens.
+     */
     function mintCap() external view returns (uint256);
 
     //functions
+    /**
+     * @notice Mints HedgeUnit tokens by transferring the equivalent amount of DS and PA tokens.
+     * @param amount The amount of HedgeUnit tokens to mint.
+     * @custom:reverts MintingPaused if minting is currently paused.
+     * @custom:reverts MintCapExceeded if the mint cap is exceeded.
+     */
     function mint(uint256 amount) external;
+
+    /**
+     * @notice Dissolves HedgeUnit tokens and returns the equivalent amount of DS and PA tokens.
+     * @param amount The amount of HedgeUnit tokens to dissolve.
+     * @return dsAmount The amount of DS tokens returned.
+     * @return paAmount The amount of PA tokens returned.
+     * @custom:reverts InvalidAmount if the user has insufficient HedgeUnit balance.
+     */
     function dissolve(uint256 amount) external returns (uint256 dsAmount, uint256 paAmount);
+
+    /**
+     * @notice Sets the paused state for minting.
+     * @param _paused The new paused state (true to pause, false to unpause).
+     * @custom:reverts InvalidValue if the paused state is not changed.
+     */
     function setMintingPaused(bool _paused) external;
+
+    /**
+     * @notice Updates the mint cap.
+     * @param _newMintCap The new mint cap value.
+     * @custom:reverts InvalidValue if the mint cap is not changed.
+     */
     function updateMintCap(uint256 _newMintCap) external;
 }
