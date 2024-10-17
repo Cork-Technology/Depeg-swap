be642f6c7a19d7ca2e9d69ef52a02d9dd4e07197
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-10-17 18:56:54 +0530

    Refactor : Added Licenses

--- contracts/core/Vault.sol
+++ contracts/core/Vault.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
 import {VaultLibrary} from "../libraries/VaultLib.sol";
--- contracts/libraries/State.sol
+++ contracts/libraries/State.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
 import {Pair} from "./Pair.sol";
