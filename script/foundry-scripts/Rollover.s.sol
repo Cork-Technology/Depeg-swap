c32475093b4442ca3acdf5ee4d9653dee7551d8f
ziankork <zian@cork.tech>
2024-10-31 16:35:06 +0800

    chore: widen pragma

--- script/foundry-scripts/Rollover.s.sol
+++ script/foundry-scripts/Rollover.s.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 import {IUniswapV2Factory} from "v2-core/interfaces/IUniswapV2Factory.sol";
 import {IUniswapV2Router02} from "v2-periphery/interfaces/IUniswapV2Router02.sol";
