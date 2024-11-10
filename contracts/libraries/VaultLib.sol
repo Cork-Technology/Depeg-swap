1f87e9caab5b1f1b452a834eb13bab272c755a52
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-11-10 16:32:51 +0530

    Refactor : code formatted

--- contracts/libraries/VaultLib.sol
+++ contracts/libraries/VaultLib.sol
@@ -62,7 +62,6 @@ library VaultLibrary {
         uint256 raTolerance,
         uint256 ctTolerance
     ) internal {
-
         IERC20(raAddress).safeIncreaseAllowance(address(ammRouter), raAmount);
         IERC20(ctAddress).safeIncreaseAllowance(address(ammRouter), ctAmount);
 
@@ -248,8 +247,7 @@ library VaultLibrary {
 
         uint256 ctRatio = __getAmmCtPriceRatio(self, flashSwapRouter, dsId);
 
-        (uint256 ra, uint256 ct, uint256 originalBalance) =
-            self.vault.pool.rationedToAmm(ctRatio);
+        (uint256 ra, uint256 ct, uint256 originalBalance) = self.vault.pool.rationedToAmm(ctRatio);
 
         // this doesn't really matter tbh, since the amm is fresh and we're the first one to add liquidity to it
         (uint256 raTolerance, uint256 ctTolerance) =
@@ -705,7 +703,9 @@ library VaultLibrary {
         }
 
         if (redeemParams.amount > ERC20(self.vault.lv._address).balanceOf(owner)) {
-            revert IVault.InsufficientBalance(owner, redeemParams.amount,ERC20(self.vault.lv._address).balanceOf(owner));
+            revert IVault.InsufficientBalance(
+                owner, redeemParams.amount, ERC20(self.vault.lv._address).balanceOf(owner)
+            );
         }
 
         self.vault.userLvBalance[owner].balance -= redeemParams.amount;
