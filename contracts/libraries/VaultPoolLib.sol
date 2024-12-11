e3ee01cb8579b60ed58d00b42c053a745433d1ec
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2024-12-11 14:21:07 +0530

    refactor : refactored reserve function for improving the readability and maintainability of code

--- contracts/libraries/VaultPoolLib.sol
+++ contracts/libraries/VaultPoolLib.sol
@@ -10,28 +10,26 @@ import {MathHelper} from "./MathHelper.sol";
  */
 library VaultPoolLibrary {
     function reserve(VaultPool storage self, uint256 totalLvIssued, uint256 addedRa, uint256 addedPa) internal {
-        uint256 totalLvWithdrawn = self.withdrawalPool.atrributedLv;
+        // new protocol amendement, no need to reserve for lv
+        uint256 totalLvWithdrawn = 0;
 
         // RA
         uint256 totalRa = self.withdrawalPool.raBalance + addedRa;
-        (uint256 attributedToWithdraw, uint256 attributedToAmm, uint256 ratePerLv) =
+        (, uint256 attributedToAmm, uint256 ratePerLv) =
             MathHelper.separateLiquidity(totalRa, totalLvIssued, totalLvWithdrawn);
 
-        self.withdrawalPool.raBalance = attributedToWithdraw;
         self.ammLiquidityPool.balance = attributedToAmm;
         self.withdrawalPool.raExchangeRate = ratePerLv;
 
         // PA
         uint256 totalPa = self.withdrawalPool.paBalance + addedPa;
-        (attributedToWithdraw, attributedToAmm, ratePerLv) =
-            MathHelper.separateLiquidity(totalPa, totalLvIssued, totalLvWithdrawn);
+        (, attributedToAmm, ratePerLv) =
+            MathHelper.separateLiquidity(totalPa, totalLvIssued, 0);
 
-        self.withdrawalPool.paBalance = attributedToAmm; 
-        self.withdrawalPool.stagnatedPaBalance = attributedToWithdraw;
+        self.withdrawalPool.paBalance = attributedToAmm;
         self.withdrawalPool.paExchangeRate = ratePerLv;
 
         assert(totalRa == self.withdrawalPool.raBalance + self.ammLiquidityPool.balance);
-        assert(totalPa == self.withdrawalPool.paBalance + self.withdrawalPool.stagnatedPaBalance);
     }
 
     function rationedToAmm(VaultPool storage self, uint256 ratio) internal view returns (uint256 ra, uint256 ct, uint256 originalBalance) {
