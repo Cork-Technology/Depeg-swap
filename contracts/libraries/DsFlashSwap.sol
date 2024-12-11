b08590ccbeaa32b01ff503cc955c4680566c6114
ziankork <zian@cork.technology>
2024-12-11 18:44:45 +0800

    fix: remove hotfix

--- contracts/libraries/DsFlashSwap.sol
+++ contracts/libraries/DsFlashSwap.sol
@@ -109,8 +109,7 @@ library DsFlashSwaplibrary {
         public
         returns (uint256 emptied)
     {
-        // TODO : hotfix
-        self.ds[dsId].lvReserve -= amount - 1;
+        self.ds[dsId].lvReserve -= amount;
         self.ds[dsId].ds.transfer(to, amount);
         emptied = amount;
     }
