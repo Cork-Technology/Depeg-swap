cd01267f3135bf5aaf6498a053b11ef7339bcfe9
Vaishnavi Singh <gaursinghvaishnavi@gmail.com>
2024-12-12 16:05:26 +0530

    refactor

--- test/forge/TestPsm.t.sol
+++ test/forge/TestPsm.t.sol
@@ -4,7 +4,7 @@ import {Helper} from "./Helper.sol";
 import {DummyWETH} from "./../../contracts/dummy/DummyWETH.sol";
 import {Id, Pair, PairLibrary} from "./../../contracts/libraries/Pair.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "forge-std/console.sol";
+import {console} from "forge-std/console.sol";
 
 contract PsmCoreTest is Helper {
     DummyWETH internal ra;
@@ -14,51 +14,60 @@ contract PsmCoreTest is Helper {
     uint256 public constant DEFAULT_DEPOSIT_AMOUNT = 10000 ether;
     uint256 public constant EXPIRY = 1 days;
 
+      event Repurchased(
+        Id indexed id,
+        address indexed buyer,
+        uint256 indexed dsId,
+        uint256 raUsed,
+        uint256 receivedPa,
+        uint256 receivedDs,
+        uint256 feePercentage,
+        uint256 fee,
+        uint256 exchangeRates
+    );
+
+
+
     address user2 = address(30);
       address public lv;
       uint256 public dsId;
 
-    function setUp() public {
+     function setUp() public {
         vm.startPrank(DEFAULT_ADDRESS);
 
         deployModuleCore();
 
-       
         (ra, pa, currencyId) = initializeAndIssueNewDs(EXPIRY, 1 ether);
-
-       
         vm.deal(DEFAULT_ADDRESS, type(uint256).max);
-        vm.stopPrank();
-        vm.startPrank(user2);
-        vm.deal(user2, type(uint256).max);
 
-       
         ra.deposit{value: type(uint128).max}();
         pa.deposit{value: type(uint128).max}();
 
         vm.stopPrank();
+        vm.startPrank(user2);
 
-        vm.startPrank(DEFAULT_ADDRESS);
-
-       
-        ra.approve(address(moduleCore), type(uint256).max);
-        pa.approve(address(moduleCore), type(uint256).max);
-
-        moduleCore.depositPsm(currencyId, DEFAULT_DEPOSIT_AMOUNT);
-
-     
-        dsId = moduleCore.lastDsId(currencyId);
+        vm.deal(user2, type(uint256).max);
+        ra.deposit{value: type(uint128).max}();
+        pa.deposit{value: type(uint128).max}();
 
         vm.stopPrank();
+        vm.startPrank(DEFAULT_ADDRESS);
+
+        // 10000 for psm 10000 for LV
+        ra.approve(address(moduleCore), type(uint256).max);
+
+        // moduleCore.depositPsm(currencyId, DEFAULT_DEPOSIT_AMOUNT);
+        moduleCore.depositLv(currencyId, DEFAULT_DEPOSIT_AMOUNT, 0, 0);
+
     }
 
     function test_depositPsm() public {
        
         vm.startPrank(DEFAULT_ADDRESS);
-        ra.approve(address(moduleCore), 1000 ether);
+        ra.approve(address(moduleCore), 1 ether);
 
       
-        (uint256 received, uint256 exchangeRate) = moduleCore.depositPsm(currencyId, 1000 ether);
+        (uint256 received, uint256 exchangeRate) = moduleCore.depositPsm(currencyId, 1 ether);
 
         assertTrue(received > 0, "Should receive tokens");
         assertEq(exchangeRate, defaultExchangeRate(), "Exchange rate should match default");
@@ -66,37 +75,14 @@ contract PsmCoreTest is Helper {
         vm.stopPrank();
     }
 
-    function test_repurchase() public {
-      
-        vm.startPrank(DEFAULT_ADDRESS);
-        pa.approve(address(moduleCore), 1000 ether);
-
-      
-        (
-            uint256 dsId,
-            uint256 receivedPa,
-            uint256 receivedDs,
-            uint256 feePercentage,
-            uint256 fee,
-            uint256 exchangeRates
-        ) = moduleCore.repurchase(currencyId, 1000 ether);
-
-        assertTrue(receivedPa > 0, "Should receive PA tokens");
-        assertTrue(receivedDs > 0, "Should receive DS tokens");
-        assertTrue(fee > 0, "Should have fee");
-
-        vm.stopPrank();
-    }
-
     function test_redeemWithDs() public {
        
         vm.startPrank(DEFAULT_ADDRESS);
-        ra.approve(address(moduleCore), 1000 ether);
+        ra.approve(address(moduleCore), type(uint256).max );
 
-        uint256 dsId = moduleCore.lastDsId(currencyId);
-
+        uint256 _dsId = moduleCore.lastDsId(currencyId);
      
-        (uint256 received, uint256 exchangeRate, uint256 fee) = moduleCore.redeemRaWithDs(currencyId, dsId, 1000 ether);
+        (uint256 received, uint256 exchangeRate, uint256 fee) = moduleCore.redeemRaWithDs(currencyId, _dsId, 1000 ether);
 
         assertTrue(received > 0, "Should receive tokens");
         assertTrue(fee > 0, "Should have fee");
@@ -111,16 +97,16 @@ contract PsmCoreTest is Helper {
 
     function test_availableForRepurchase() public {
         
-        moduleCore.depositPsm(currencyId, 1000 ether);
+        moduleCore.depositPsm(currencyId, 1 ether);
 
-        (uint256 pa, uint256 ds, uint256 dsId) = moduleCore.availableForRepurchase(currencyId);
+        (uint256 _pa, uint256 ds, uint256 _dsId) = moduleCore.availableForRepurchase(currencyId);
 
-        assertTrue(pa > 0, "PA should be available");
+        assertTrue(_pa > 0, "PA should be available");
         assertTrue(ds > 0, "DS should be available");
-        assertTrue(dsId > 0, "DS ID should be valid");
+        assertTrue(_dsId > 0, "DS ID should be valid");
     }
 
     function defaultExchangeRate() internal pure override returns (uint256) {
-        return 1.5 ether;
+        return 1.0 ether;
     }
 }
