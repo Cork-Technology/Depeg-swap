4568529b38310b17d0520aeafed6046afbcc4f21
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2025-01-03 13:27:52 +0530

    deploy : added script for flahswap and swapping operations

--- script/foundry-scripts/SwapRaForDs.s.sol
+++ script/foundry-scripts/SwapRaForDs.s.sol
@@ -22,15 +22,15 @@ contract SwapRAForDSScript is Script {
     bool public isProd = vm.envBool("PRODUCTION");
     uint256 public base_redemption_fee = vm.envUint("PSM_BASE_REDEMPTION_FEE_PERCENTAGE");
     uint256 public pk = vm.envUint("PRIVATE_KEY");
-    address deployer = 0x036febB27d1da9BFF69600De3C9E5b6cd6A7d275;
-    address ceth = 0x11649B3aEc3D4Cd35D0727D786c234329B756fd9;
-    address cusd = 0x4c82BdeDD41bf0284fd6BCa1b6A317fEF6A6d237;
-    address bsETHAdd = 0x2019e2E0D0DE78b65ce698056EAE468192b40daC;
-    address wamuETHAdd = 0x81EcEa063eB1E477365bd6c0AE7E1d1f3d84442E;
-    address mlETHAdd = 0xD1813fD95E557d273E8009db91C6BC412F56eE56;
-    address svbUSDAdd = 0xeD273d746bC1CefA9467ea5e81e9cd22eaC27397;
-    address fedUSDAdd = 0xEBdc16512a8c79c39EB27cc27e387039AF573f82;
-    address omgUSDAdd = 0x42B025047A12c403803805195230C257D2170Bb1;
+    address deployer = 0xBa66992bE4816Cc3877dA86fA982A93a6948dde9;
+    address ceth = 0xbcD4B73511328Fd44416ce7189eb64F063DA5F41;
+    address cusd = 0xA69b095360F2DD024Ff0571bA12D9CA6823D2C0b;
+    address bsETHAdd = 0xaF4acbB6e9E7C13D8787a60C199462Bc3095Cad7;
+    address wamuETHAdd = 0xC9eF4a21d0261544b10CC5fC9096c3597daaA29d;
+    address mlETHAdd = 0x2B56646D79375102b5aaaf3c228EE90DE2913d5E;
+    address svbUSDAdd = 0xBF578784a7aFaffE5b63C60Ed051E55871B7E114;
+    address fedUSDAdd = 0xa4A181100F7ef4448d0d34Fd0B6Dc17ecE5C1442;
+    address omgUSDAdd = 0x34f49a5b81B61E91257460E0C6c168Ccee86a4b1;
 
     uint256 wamuETHExpiry = 3.5 days;
     uint256 bsETHExpiry = 3.5 days;
@@ -52,20 +52,19 @@ contract SwapRAForDSScript is Script {
         vm.startBroadcast(pk);
         vm.pauseGasMetering();
 
-        moduleCore = ModuleCore(0x3390573A8Cd1aB9CFaE5e1720e4e7867Ed074a38);
-        routerState = RouterState(0x96EE05bA5F2F2D3b4a44f174e5Df3bba1B9C0D17);
+        moduleCore = ModuleCore(0xc5f00EE3e3499e1b211d1224d059B8149cD2972D);
+        routerState = RouterState(0xC89c5b91d6389FDDa8A0Ee29dc2eFC7330Ee42A1);
         console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
-        Assets[6] memory assets = [mlETH, bsETH, wamuETH, svbUSD, fedUSD, omgUSD];
-        // Assets[3] memory assets = [mlETH, bsETH, omgUSD];
+        // Assets[6] memory assets = [mlETH, bsETH, wamuETH, svbUSD, fedUSD, omgUSD];
+        Assets[3] memory assets = [mlETH, bsETH, wamuETH];
         // Assets[1] memory assets = [fedUSD];
 
         for (uint256 i = 0; i < assets.length; i++) {
-
-            for (uint256 j = 0; j < 100; j++) {
-                console.log("Swapping iteration: ", j);
+            // for (uint256 j = 0; j < 100; j++) {
+                // console.log("Swapping iteration: ", j);
                 swapRaForDs(assets[i], 0.01 ether);
-            }
+            // }
         }
 
         console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
@@ -76,11 +75,17 @@ contract SwapRAForDSScript is Script {
         Id reserveId = moduleCore.getId(asset.peggedAsset, asset.redemptionAsset, asset.expiryInterval);
         Id id = PairLibrary.toId(PairLibrary.initalize(asset.peggedAsset, asset.redemptionAsset, asset.expiryInterval));
         uint256 dsId = moduleCore.lastDsId(id);
+        (,address dsToken) = moduleCore.swapAsset(id, dsId);
 
         CETH(asset.redemptionAsset).approve(address(routerState), swapAmt);
         IDsFlashSwapCore.BuyAprroxParams memory params =
             IDsFlashSwapCore.BuyAprroxParams(108, 108, 1 ether, 1 gwei, 1 gwei, 0.01 ether);
         routerState.swapRaforDs(reserveId, dsId, swapAmt, 0, params);
+        console.log("Swap RA for DS");
+
+
+        CETH(dsToken).approve(address(routerState), swapAmt);
+        routerState.swapDsforRa(reserveId, dsId, swapAmt, 0);
         console.log("Swap RA for DS");
         console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
     }
