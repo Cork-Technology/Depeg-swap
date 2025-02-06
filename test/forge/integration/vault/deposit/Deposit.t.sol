46a1925bbea63a4ab5efc80e9c3405ba0190826d
ziankork <zian@cork.technology>
2025-02-06 16:31:44 +0800

    test: added 18 decimal test

--- test/forge/integration/vault/deposit/Deposit.t.sol
+++ test/forge/integration/vault/deposit/Deposit.t.sol
@@ -305,4 +305,41 @@ contract DepositTest is Helper {
         vm.expectRevert();
         received = moduleCore.depositLv(id, amount, 100000 ether, 10000 ether);
     }
+
+    function test_depositInflation() external {
+        uint256 depositAmount = 2;
+
+        uint256 adjustedDepositAmount = depositAmount;
+
+        VaultBalances memory balances = moduleCore.getVaultBalances(defaultCurrencyId);
+        vm.assertEq(balances.ra.locked, 0);
+        vm.assertEq(balances.ctBalance, 0);
+
+        uint256 received = moduleCore.depositLv(defaultCurrencyId, adjustedDepositAmount, 0, 0);
+
+        balances = moduleCore.getVaultBalances(defaultCurrencyId);
+        vm.assertEq(balances.ra.locked, 0);
+
+        // default split is 50/50
+        vm.assertApproxEqAbs(balances.ctBalance, depositAmount / 2, depositAmount / 100);
+
+        uint256 dsReserve = flashSwapRouter.getLvReserve(defaultCurrencyId, 1);
+        // ~0.5 from split, ~0.2 from AMM with some precision tolerance
+        vm.assertApproxEqAbs(dsReserve, depositAmount * 3 / 4, depositAmount / 100);
+
+        vm.assertEq(received, 1);
+
+        (received,) = moduleCore.depositPsm(defaultCurrencyId, 1 ether);
+
+        ICorkHook ammRouter = moduleCore.getAmmRouter();
+        ra.approve(address(ammRouter), 1 ether);
+        IERC20(ct).approve(address(ammRouter), 1 ether);
+        (,, uint256 minted) = ammRouter.addLiquidity(address(ra), ct, 1 ether, 1 ether, 0, 0, block.timestamp);
+
+        vm.assertGe(minted, 1e9);
+        IERC20(ammRouter.getLiquidityToken(address(ra), ct)).transfer(address(moduleCore), minted);
+
+        received = moduleCore.depositLv(defaultCurrencyId, 1 ether, 0, 0);
+        vm.assertGe(received, 100);
+    }
 }
