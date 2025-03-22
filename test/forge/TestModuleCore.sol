d75f2f69572b40ad92632198266899b4ca630559
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2025-03-22 12:40:35 +0530

    fix : fixed compilation error in V1-Router repo

--- test/forge/TestModuleCore.sol
+++ test/forge/TestModuleCore.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.24;
 
-import {ModuleCore} from "contracts/core/ModuleCore.sol";
-import {Id, Pair} from "contracts/libraries/Pair.sol";
+import {ModuleCore} from "../../contracts/core/ModuleCore.sol";
+import {Id, Pair} from "../../contracts/libraries/Pair.sol";
 import {
     State,
     PsmPoolArchive,
@@ -11,11 +11,11 @@ import {
     VaultConfig,
     VaultBalances,
     VaultWithdrawalPool
-} from "contracts/libraries/State.sol";
+} from "../../contracts/libraries/State.sol";
 import {BitMaps} from "@openzeppelin/contracts/utils/structs/BitMaps.sol";
-import {DepegSwap} from "contracts/libraries/DepegSwapLib.sol";
-import {LvAsset} from "contracts/libraries/LvAssetLib.sol";
-import {StateView} from "contracts/core/StateView.sol";
+import {DepegSwap} from "../../contracts/libraries/DepegSwapLib.sol";
+import {LvAsset} from "../../contracts/libraries/LvAssetLib.sol";
+import {StateView} from "../../contracts/core/StateView.sol";
 
 /// @title TestModuleCore Contract, used for testing ModuleCore contract, mostly here for getter functions
 contract TestModuleCore is ModuleCore, StateView {}
