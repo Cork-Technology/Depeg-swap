3affc501548e34d8f90eeb2a9a535b8cf6466f1e
ziankork <zian@cork.technology>
2025-04-09 16:38:39 +0800

    feat: refactor factory

--- contracts/core/oracles/CTOracleFactory.sol
+++ contracts/core/oracles/CTOracleFactory.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.24;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {CTOracle} from "./CTOracle.sol";
+import {CtOracle} from "./CTOracle.sol";
+import {LegacyCtOracle} from "./LegacyCTOracle.sol";
 import {ICTOracleFactory} from "./../../interfaces/ICTOracleFactory.sol";
+import {Id} from "./../../libraries/Pair.sol";
 
 /**
  * @title Factory contract for CT Oracles
@@ -12,12 +14,14 @@ import {ICTOracleFactory} from "./../../interfaces/ICTOracleFactory.sol";
  * @notice Factory contract for deploying CT Oracles contracts
  */
 contract CTOracleFactory is OwnableUpgradeable, UUPSUpgradeable, ICTOracleFactory {
+    /// @notice Mapping from CT token address to oracle address
+    mapping(address => address) public ctToOracle;
+
+    address public moduleCore;
+
     /// @notice __gap variable to prevent storage collisions
     // slither-disable-next-line unused-state
     uint256[49] private __gap;
-
-    /// @notice Mapping from CT token address to oracle address
-    mapping(address => address) public ctToOracle;
 
     constructor() {
         _disableInitializers();
@@ -27,12 +31,14 @@ contract CTOracleFactory is OwnableUpgradeable, UUPSUpgradeable, ICTOracleFactor
      * @notice Initializes the factory contract
      * @param _owner The owner of the factory contract
      */
-    function initialize(address _owner) external initializer {
+    function initialize(address _owner, address _moduleCore) external initializer {
         if (_owner == address(0)) {
             revert ZeroAddress();
         }
         __Ownable_init(_owner);
         __UUPSUpgradeable_init();
+
+        moduleCore = _moduleCore;
     }
 
     /**
@@ -45,22 +51,36 @@ contract CTOracleFactory is OwnableUpgradeable, UUPSUpgradeable, ICTOracleFactor
      * @inheritdoc ICTOracleFactory
      */
     function createCTOracle(address _ctToken) external returns (address oracle) {
+        _verifyParams(_ctToken);
+
+        // Deploy new oracle
+        oracle = address(new CtOracle(moduleCore, _ctToken));
+
+        _storeAndEmit(_ctToken, oracle);
+    }
+
+    function _verifyParams(address _ctToken) internal view {
         if (_ctToken == address(0)) {
             revert ZeroAddress();
         }
 
-        // TODO not sure if this is needed, need to confirm with heri wheter only 1 oracle is allowed to exist
-        // Check if oracle for this CT token already exists
         if (ctToOracle[_ctToken] != address(0)) {
             revert OracleAlreadyExists();
         }
+    }
 
-        // Deploy new oracle
-        oracle = address(new CTOracle(address(this)));
-
-        // Store the oracle address
+    function _storeAndEmit(address _ctToken, address oracle) internal {
         ctToOracle[_ctToken] = oracle;
 
         emit CTOracleCreated(_ctToken, oracle);
+    }
+
+    function createCTOracle(address _ctToken, Id marketId) external returns (address oracle) {
+        _verifyParams(_ctToken);
+
+        // Deploy new oracle
+        oracle = address(new LegacyCtOracle(moduleCore, _ctToken, marketId));
+
+        _storeAndEmit(_ctToken, oracle);
     }
 }
