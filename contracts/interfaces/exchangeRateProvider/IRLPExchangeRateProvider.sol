f2a10d1edf0012f227c8a4537f7fe401e2e813d4
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2025-04-11 15:00:18 +0530

    test: Added tests for RLPExchangeRateProvider

--- contracts/interfaces/exchangeRateProvider/IRLPExchangeRateProvider.sol
+++ contracts/interfaces/exchangeRateProvider/IRLPExchangeRateProvider.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
+import {Id} from "../../libraries/Pair.sol";
+
 /**
  * @title IRLPExchangeRateProvider Interface
  * @author Cork Team
@@ -8,6 +10,7 @@ pragma solidity ^0.8.24;
  */
 interface IRLPExchangeRateProvider {
     function rate() external view returns (uint256);
+    function rate(Id id) external view returns (uint256);
 }
 
 interface IUSRPriceOracle {
