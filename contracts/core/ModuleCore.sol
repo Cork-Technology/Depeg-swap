59b115db929ae23594c66f9a48730702540e6cfc
ziankork <zian@cork.technology>
2025-04-25 14:29:52 +0800

    feat: add storage vars

--- contracts/core/ModuleCore.sol
+++ contracts/core/ModuleCore.sol
@@ -23,7 +23,8 @@ import {AmmId, toAmmId} from "Cork-Hook/lib/State.sol";
 contract ModuleCore is OwnableUpgradeable, UUPSUpgradeable, PsmCore, Initialize, VaultCore {
     /// @notice __gap variable to prevent storage collisions
     // slither-disable-next-line unused-state
-    uint256[49] private __gap;
+    // upgrade to add access control for pausing PA liquidation
+    uint256[48] private __gap;
 
     using PsmLibrary for State;
     using PairLibrary for Pair;
